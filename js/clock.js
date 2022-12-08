c4f1532ccebf78f443bdfb029565eace5dab5865
stevencol <steven2088hotmail.es>
2022-12-08 00:46:53 -0500

    Se Crea logica

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,5 @@
 var milesima = 0;
-var segundo = 0;
+var segundo = 00;
 var minuto = 0;
 var hora = 0;
 var segundoP = ":";
@@ -13,45 +13,117 @@ var dMilesima = document.getElementById("milesima");
 var segundoP = document.getElementById("segundoP");
 var botonStart = document.getElementById("start");
 var botonReset = document.getElementById("reset");
+var botonPause = document.getElementById("pause");
 
 
 dHora.innerHTML = hora;
-dMinito.innerHTML = hora;
-dSegundo.innerHTML = hora;
-dMilesima.innerHTML = hora;
+dMinito.innerHTML = minuto;
+dSegundo.innerHTML = segundo;
+dMilesima.innerHTML = milesima;
 
 
 
-function startTime
-    () {
+function startTime() {
     if (localStorage.getItem("milesima") == null) {
         localStorage.setItem("milesima", milesima);
+        localStorage.setItem("segundo", segundo);
+        localStorage.setItem("minuto", minuto);
+        localStorage.setItem("hora", hora)
+    }else{
+         milesima =parseInt(localStorage.getItem("milesima"));
+         segundo =parseInt(localStorage.getItem("segundo"));
+         minuto =parseInt(localStorage.getItem("minuto"));
+         hora =parseInt(localStorage.getItem("hora"));
     }
+
 
     milesima = parseInt(localStorage.getItem("milesima")) + 1;
     localStorage.setItem("milesima", milesima);
     dMilesima.innerHTML = milesima;
     console.log(milesima);
 
+
+    if (milesima == 10) {
+        segundo = segundo + 1;
+        milesima = 0;
+        localStorage.setItem("segundo", segundo);
+        localStorage.setItem("milesima", milesima);
+        dSegundo.innerHTML = parseInt(localStorage.getItem("segundo"));
+        dMilesima.innerHTML =parseInt(localStorage.getItem("milesima"));
+       
+
+        if (segundo == 60) {
+            segundo = 0;
+            minuto = minuto + 1;
+            localStorage.setItem("segundo", segundo);
+            localStorage.setItem("milesima", milesima);
+            localStorage.setItem("minuto", minuto);
+            dSegundo.innerHTML = segundo;
+            dMilesima.innerHTML = milesima;
+            dMinito.innerHTML = minuto;
+
+
+        }
+
+        if (minuto == 60) {
+            hora = hora + 1;
+            segundo = 0;
+            minuto = 0;
+            localStorage.setItem("segundo", segundo);
+            localStorage.setItem("milesima", milesima);
+            localStorage.setItem("minuto", minuto);
+            localStorage.setItem("hora", hora);
+            dSegundo.innerHTML = parseInt(localStorage.getItem("segundo"));segundo;
+            dMilesima.innerHTML =parseInt(localStorage.getItem("milesima")); ;
+            dMinito.innerHTML =parseInt(localStorage.getItem("minuto")); ;
+            dHora.innerHTML = parseInt(localStorage.getItem("hora"));;
+
+
+        }
+    }
+
 }
 
 
 
-function star(){
-    setInterval(startTime, 1000);
+function star() {
+    clock = setInterval(startTime, 100);
 }
 
 
-botonStart.onclick= star;
+botonStart.onclick = star;
 
 
 
 
 
 
- function reset(){
-    milesima=0;
+function reset() {
+    milesima = 0;
     localStorage.setItem("milesima", milesima)
- }
+    dMilesima.innerHTML = milesima;
 
- botonStart.onclick= reset;
+
+    segundo = 0;
+    localStorage.setItem("segundo", segundo)
+    dSegundo.innerHTML = segundo;
+
+    minuto = 0;
+    localStorage.setItem("minuto", minuto)
+    dSegundo.innerHTML = minuto;
+
+    hora = 0;
+    localStorage.setItem("hora", hora)
+    hora.innerHTML = hora;
+
+    clearInterval(clock);
+
+}
+
+function pause(){
+    clearInterval(clock);
+}
+
+
+botonReset.onclick = reset;
+botonPause.onclick = pause;
